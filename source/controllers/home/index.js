f48b1441b16add4a5d5b4baa7a25231cffca0a1d
Tam Pham <tampham47@live.com>
2015-03-25 18:33:12 +0700

    create html fields.

--- source/controllers/home/index.js
+++ source/controllers/home/index.js
@@ -6,7 +6,13 @@ angular.module('site.home', []).config(function($routeProvider) {
     templateUrl: 'views/home/index.html'
   });
 })
-
 .controller('HomePageCtrl', function($scope, $location) {
   console.log('HomePageCtrl');
+
+  $('[data-toggle="popover"]').popover({
+    content: function(){
+      return $('.pop-content', this).html();
+    }
+  });
+
 });
